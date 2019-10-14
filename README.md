f69746ab10ae147f7cf594f8ffb857fe7c4a627a
S0LERA <pablo.rodriguez22@alu.uclm.es>
2019-10-14 16:26:29 +0200

    Versión 1.0

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Repositorio para la p1 de seguridad en redes, correspondiente con un escaner de puertos.
 
 ### Versiones
-## Version 0.5
+## Versión 0.5
  - Primer commit.
  - Se piden los datos.
+## Versión 1.0
+ - Funciona el escaneo simple si el puerto está abierto.
+ - Codigo reestructurado.
--- port_discover.py
+++ port_discover.py
@@ -7,25 +7,49 @@ def pedirDatos():
         print("Selecciona una opción:")
         print("1. Escaneo de un puerto concreto")
         print("2. Escaneo de un rango de puertos")
-        opcion = int(input())
-        if(opcion == 1 or opcion == 2):
+        entrada = int(input())
+        if(entrada == 1 or entrada == 2):
             break    
-    return opcion
+    return entrada
 
-def modo(opcion):
-    if(opcion == 1):
-        puerto_escaneo = input("Introduce el puerto que quieres escanear")
+def modo(entrada):
+    if(entrada == 1):
+        puerto_escaneo = input("Introduce el puerto que quieres escanear: ")
         ip_escaneo = input("Introduce la dirección que quieres escanear: ")
-        print("Ip: " + ip_escaneo + " ,puerto: "+ puerto_escaneo)
-    elif (opcion == 2):
-        puerto_inicio = input("Introduce el puerto de inicio ")
-        puerto_fin = input("Introduce el puerto de fin")
+        swescaner(ip_escaneo, puerto_escaneo,None)
+    elif(entrada == 2):
+        puerto_inicio = input("Introduce el puerto de inicio: ")
+        puerto_fin = input("Introduce el puerto de fin: ")
         ip_escaneo = input("Introduce la dirección que quieres escanear: ")
-        print("Ip: " + ip_escaneo + " ,puerto inicio: "+ puerto_inicio + " ,puerto fin: " + puerto_fin)
+        swescaner(ip_escaneo, puerto_inicio, puerto_fin)
 
+def swescaner(ip,p_inicio,p_fin):
+    if(p_fin == None):
+        respuesta = escaner(ip,p_inicio)
+        if(respuesta == True):
+            print("El puerto "+p_inicio+" está abierto en "+ip)
+        else:
+            print("El puerto "+p_inicio+" está cerrado en "+ip)
+    else:
+        for puerto in range(int(p_inicio),int(p_fin)):
+            respuesta = escaner(ip,puerto)
+            if(respuesta == True):
+                print("El puerto "+puerto+" está abierto en "+ip)
+
+
+def escaner(ip,puerto):
+    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        respuesta = skt.connect((ip, int(puerto)))
+        skt.close()
+        return True
+    except:
+        skt.close()
+        return False
+    
 def main():
     print("Bienvenido al escaner de puertos")
-    opcion = pedirDatos()
-    modo(opcion)
+    entrada = pedirDatos()
+    modo(entrada)
 
 main()
